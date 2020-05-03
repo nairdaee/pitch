34a8ebc22414e2f6e017c0853ba423070b6486e7
Adrian Etenyi <etadriano2@gmail.com>
2020-05-03 17:02:56 +0300

    created 3 classes used for production,development and also debugging

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+class Config:
+     '''
+    General configuration parent class
+    '''
+    pass
+
+class ProdConfig(Config):
+    '''
+    Pruduction  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+     '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+    
