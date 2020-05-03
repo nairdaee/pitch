55ea298543b1db21b8a743c06b85d50caab802a4
Adrian Etenyi <etadriano2@gmail.com>
2020-05-03 23:07:40 +0300

    add email configurations

--- config.py
+++ config.py
@@ -5,9 +5,16 @@ class Config:
     General configuration parent class
     '''
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.urandom(69)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-   
+    
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 class ProdConfig(Config):
     '''
     Pruduction  configuration child class
