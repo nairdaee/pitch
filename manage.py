64852870ae239bfb48bfe59164612525ee475e77
Adrian Etenyi <etadriano2@gmail.com>
2020-05-03 20:12:42 +0300

    update my dictionary

--- manage.py
+++ manage.py
@@ -9,6 +9,6 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User, Comment = Comment, Pitch = Pitch)
 if __name__ == '__main__':
     manager.run()
